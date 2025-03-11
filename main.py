04820b90faf56aa5804044d3ad2246e88e82f377
shoaibaziz <shoaib.aziz.space@gmail.com>
2025-03-11 11:42:37 +0300

    Implemented basic game loop with pygame initialization

---
+++ main.py
@@ -0,0 +1,26 @@
+import pygame
+from constants import *
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+
+# welcome_message = welcome()
+# print(welcome_message)    
+
+if __name__ == "__main__":
+    main()
+    
+print(f"Screen width: {SCREEN_WIDTH}")
+print(f"Screen height: {SCREEN_HEIGHT}") 
+
+
+
+
+     
